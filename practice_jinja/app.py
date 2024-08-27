91952e1a3f4abb5d19a270fcafebd8b91c771750
nicoopenna <npenna@frba.utn.edu.ar>
2024-08-26 23:04:56 -0300

    Macros

--- practice_jinja/app.py
+++ practice_jinja/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request
 
 def create_app():
     app = Flask(__name__)
+    todos = [
+        ("Get milk", False),
+        ("Learn Programming", True)
+    ]
     @app.route("/todo")
     def todo():
-        return render_template("home.html", todos=["Get milk", "Learn programming"])
+        return render_template("home.html", todos=todos)
     
     return app
