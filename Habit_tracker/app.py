51612cb9723abfe5330e8b936508b4c7dbcca4d4
nicoopenna <npenna@frba.utn.edu.ar>
2024-08-30 18:34:51 -0300

    Mongo DB

--- Habit_tracker/app.py
+++ Habit_tracker/app.py
@@ -1,17 +1,14 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask
+from routes import pages
+from pymongo import MongoClient
+from dotenv import  load_dotenv
 
-app = Flask(__name__)
-habits = ["Test habit"]
+load_dotenv()
 
-
-@app.route("/")
-def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.form:
-        habits.append(request.form.get("habit"))
-
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client.get_database("habits_tracker")
+    app.register_blueprint(pages)
+    return app
