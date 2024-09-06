5b442ca74ee9c8e13c0ad8bb2698ac7ff21ebcfe
nicoopenna <npenna@frba.utn.edu.ar>
2024-09-05 23:23:27 -0300

    Forms forms and forms

--- movie_watchlist/movie_library/routes.py
+++ movie_watchlist/movie_library/routes.py
@@ -1,5 +1,7 @@
 import uuid
+import datetime
 from dataclasses import asdict
+
 from flask import (
     Blueprint,
     current_app,
@@ -9,19 +11,30 @@ from flask import (
     url_for,
     request,
 )
-from movie_library.forms import MovieForm
+from movie_library.forms import MovieForm, ExtendedMovieForm
 from movie_library.models import Movie
+
 
 pages = Blueprint(
     "pages", __name__, template_folder="templates", static_folder="static"
 )
 
 
+@pages.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+
+    return redirect(request.args.get("current_page"))
+
+
 @pages.route("/")
 def index():
     movie_data = current_app.db.movie.find({})
     movies = [Movie(**movie) for movie in movie_data]
-
     return render_template(
         "index.html",
         title="Movies Watchlist",
@@ -43,15 +56,50 @@ def add_movie():
 
         current_app.db.movie.insert_one(asdict(movie))
 
-        return redirect(url_for(".index"))
+        return redirect(url_for(".movie", _id=movie._id))
+
+    return render_template(
+        "new_movie.html", title="Movies Watchlist - Add Movie", form=form
+    )
 
 
-@pages.get("/toggle-theme")
-def toggle_theme():
-    current_theme = session.get("theme")
-    if current_theme == "dark":
-        session["theme"] = "light"
-    else:
-        session["theme"] = "dark"
+@pages.get("/movie/<string:_id>")
+def movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
+    return render_template("movie_details.html", movie=movie)
 
-    return redirect(request.args.get("current_page"))
+
+@pages.get("/movie/<string:_id>/rate")
+def rate_movie(_id):
+    rating = int(request.args.get("rating"))
+    current_app.db.movie.update_one({"_id": _id}, {"$set": {"rating": rating}})
+
+    return redirect(url_for(".movie", _id=_id))
+
+
+@pages.get("/movie/<string:_id>/watch")
+def watch_today(_id):
+    current_app.db.movie.update_one(
+        {"_id": _id}, {"$set": {"last_watched": datetime.datetime.today()}}
+    )
+
+    return redirect(url_for(".movie", _id=_id))
+
+
+@pages.route("/edit/<string:_id>", methods=["GET", "POST"])
+def edit_movie(_id: str):
+    movie = Movie(**current_app.db.movie.find_one({"_id": _id}))
+    form = ExtendedMovieForm(obj=movie)
+    if form.validate_on_submit():
+        movie.title = form.title.data
+        movie.director = form.director.data
+        movie.year = form.year.data
+        movie.cast = form.cast.data
+        movie.series = form.series.data
+        movie.tags = form.tags.data
+        movie.description = form.description.data
+        movie.video_link = form.video_link.data
+
+        current_app.db.movie.update_one({"_id": movie._id}, {"$set": asdict(movie)})
+        return redirect(url_for(".movie", _id=movie._id))
+    return render_template("movie_form.html", movie=movie, form=form)
